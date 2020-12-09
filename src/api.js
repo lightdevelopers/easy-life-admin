1a885093cb663d3e4d69ce48e097e700aedf507d
waiapps <waiapps@waiapps.Kali>
2020-12-09 18:38:41 +0500

    Latest

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export default (path) => {
-    return ("https://localhost:8088"+path)
+    return ("https://cors-anywhere.herokuapp.com/http://ec2-3-23-63-206.us-east-2.compute.amazonaws.com:8088"+path)
 }
 
 //https://cors-anywhere.herokuapp.com/http://ec2-3-23-63-206.us-east-2.compute.amazonaws.com:8088
