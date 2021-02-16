ef58a29e8ef258cec48265c0e34ed8cc8870e900
lightdeveloper <62260921+lightdevelopers@users.noreply.github.com>
2021-02-16 22:31:56 +0500

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export default (path) => {
-    return ("https://cors-anywhere.herokuapp.com/http://ec2-13-212-152-90.ap-southeast-1.compute.amazonaws.com:8088"+path)
+    return ("https://thingproxy.freeboard.io/fetch/http://ec2-13-212-152-90.ap-southeast-1.compute.amazonaws.com:8088"+path)
 }
 
-//https://cors-anywhere.herokuapp.com/http://ec2-54-179-226-46.ap-southeast-1.compute.amazonaws.com:8088
+//http://ec2-54-179-226-46.ap-southeast-1.compute.amazonaws.com:8088
 
 export const headers = {
             "Content-Type": "application/json",
